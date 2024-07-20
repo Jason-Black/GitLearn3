3ff796479de8e9f8f09503e5131e4162f392a12b
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-07-20 18:05:28 +0000

    adding new html and refresh tailwinds to see if work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '/workspaces/GitLearn3/public/index.html', // Include all HTML files in the public directory
+    '/workspaces/GitLearn3/index.html', // Include all HTML files in the public directory
     '/workspaces/GitLearn3/src/**/*.{js,jsx,ts,tsx,css}' // Include all relevant files in the src directory
   ],
   theme: {
