b79037f4ab5bb052e787f38f88a0cd55d59f736c
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-07-20 17:37:45 +0000

    got the damn utility class to load

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '/workspaces/GitLearn3/public/index.html', // Include all HTML files in the public directory
+    '/workspaces/GitLearn3/src/**/*.{js,jsx,ts,tsx,css}' // Include all relevant files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
